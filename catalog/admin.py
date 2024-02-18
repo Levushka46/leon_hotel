6ba365865525750a51304acaa0b706be0a544a27
Lev Sergeev <levushka46@mail.ru>
2024-02-18 17:29:42 +0300

    Add user to admin panel

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import User
 
 # Register your models here.
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("id", "username", "first_name")
+    search_fields = ("first_name", "last_name", "email", "id")
